8e112ddb079bb651c6ce9925227be4413dfa0371
Siri Sandnes <siri.b.sandnes@gmail.com>
2024-04-12 00:34:30 +0200

    Initial Commit

---
+++ Front End/Front_EndApp.swift
@@ -0,0 +1,17 @@
+//
+//  Front_EndApp.swift
+//  Front End
+//
+//  Created by Siri Sandnes on 11/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Front_EndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
