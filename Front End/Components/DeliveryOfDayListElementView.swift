72fcfc8882e430da755d52b8059efb54f743f523
Siri Sandnes <siri.b.sandnes@gmail.com>
2024-04-12 04:13:37 +0200

    fixed structure, added components to reuse elements

---
+++ Front End/Components/DeliveryOfDayListElementView.swift
@@ -0,0 +1,18 @@
+//
+//  DeliveryOfDayListElementView.swift
+//  Front End
+//
+//  Created by Siri Sandnes on 12/04/2024.
+//
+
+import SwiftUI
+
+struct DeliveryOfDayListElementView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeliveryOfDayListElementView()
+}
