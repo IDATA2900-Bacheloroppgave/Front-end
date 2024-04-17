112c2adb0d69f61159223b765df769b6a6499033
Ina Folland Hegg <inafo@stud.ntnu.no>
2024-04-17 16:31:48 +0200

    Started implementing the settings view

--- Front End/View/Settings/SettingsView.swift
+++ Front End/View/Settings/SettingsView.swift
@@ -12,18 +12,117 @@ struct SettingsView: View {
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        ZStack{
-            Color(red: 0.96, green: 0.96, blue: 0.96)
-            Button(action: {
+        NavigationStack {
+            ZStack{
                 
-                if userStateViewModel.logout(){
-                    dismiss()
+                VStack(alignment: .leading, spacing: 20) {
+                    VStack {
+                        Text("Settings")
+                            .font(.system(size: 22))
+                            .frame(maxWidth: .infinity)
+                            .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
+                            .background(.yellow)
+                        
+                        List{
+                            NavigationLink{
+                                EditProfileView()
+                            }label: {
+                                HStack{
+                                    Text("IF")
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                        .frame(width: 48, height: 48)
+                                        .background(Color(.yellow).opacity(0.5))
+                                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                                    
+                                    VStack (alignment: .leading, spacing: 5) {
+                                        Text("Ina Folland")
+                                            .font(.headline)
+                                            .fontWeight(.semibold)
+                                        
+                                        Text("ina@gmail.com")
+                                            .font(.body)
+                                            .foregroundStyle(.gray)
+                                            .padding(.leading, 2)
+                                            .tint(.gray)
+                                        
+                                    }}
+                            }
+                            .padding(.init(top: 15, leading: 0, bottom: 15, trailing: 0))
+                            HStack{
+                                
+                                Text("Store:")
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                
+                                Spacer()
+                                
+                                Text("Rema Averøy")
+                                    .font(.body)
+                                    .foregroundStyle(.gray)
+                                    .padding(.leading, 2)
+                                    .tint(.gray)
+                                
+                            }
+                            .padding(.init(top: 15, leading: 10, bottom: 15, trailing: 10))
+                            NavigationLink{
+                                ChangePasswordView()
+                            }label: {
+                                HStack{
+                                    
+                                    VStack (alignment: .leading, spacing: 5) {
+                                        
+                                        Text("Change password")
+                                            .font(.subheadline)
+                                            .fontWeight(.semibold)
+                                        
+                                    }
+                                }
+                                .padding(.init(top: 15, leading: 10, bottom: 15, trailing: 0))
+                            }}
+                        
+                        
+                        Button(action: {
+                            if userStateViewModel.logout() {
+                                dismiss()
+                            }
+                            
+                        }) {
+                            HStack {
+                                Text("Log out")
+                                    .font(.title2)
+                                Spacer()
+                                Image(systemName: "rectangle.portrait.and.arrow.right")
+                                    .resizable()
+                                    .frame(width: 35, height: 35)
+                                
+                                
+                            }
+                            .foregroundColor(.black)
+                            .padding()
+                            .background(Color.yellow)
+                            .cornerRadius(5)
+                        }
+                        .padding()
+                        
+                    }
                 }
-            }, label: {
-                Text("Log out")
-                    .foregroundStyle(Color.black)
-            })
+            }
         }
+    }
+}
+
+struct EditProfileView: View {
+    var body: some View {
+        // Your form fields here
+        Text("Edit Profile View")
+    }
+}
+
+struct ChangePasswordView: View {
+    var body: some View {
+        // Your form fields here
+        Text("Change Password View")
     }
 }
 
